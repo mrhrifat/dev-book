dd2c195a9a40ecceeaee8a76eac2b6646e7a4843
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 16:57:32 +0600

    build: dev text, icon, searchbox & user box

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,60 @@
 import React from "react";
-import { AppBar, Toolbar, Container, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  styled,
+  Badge,
+  InputBase,
+  Box,
+  Avatar,
+} from "@mui/material";
+import { LogoDev, Mail, Notifications } from "@mui/icons-material";
+
+const CustomToolbar = styled(Toolbar)({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "#fff",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  gap: "20px",
+  //   justifyContent: "space-evenly",
+}));
 
 const Navbar = () => {
   return (
     <AppBar position="sticky">
-      <Toolbar>
-        <Container>
-        <Typography variant="h5">Dev Book</Typography>
-        </Container>
-      </Toolbar>
+      <CustomToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", md: "block" } }}>
+          Dev Book
+        </Typography>
+        <LogoDev sx={{ display: { xs: "block", md: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <UserBox>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={3} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Jhon Doe"
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"
+            sx={{ width: 25, height: 25 }}
+          />
+        </UserBox>
+      </CustomToolbar>
     </AppBar>
   );
 };
