9fd13b0847e55f0f1c291db3c1caba98ad2cf3e2
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 17:54:36 +0600

    build: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,75 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { Home } from "@mui/icons-material";
 
 const Sidebar = () => {
   return (
-    <Box
-      flex={1}
-      bgcolor="coral"
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
