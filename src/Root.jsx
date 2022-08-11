827f4dd146d3211190ae475101dcc9176daea0a3
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 15:46:53 +0600

    build: import root component

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
 
 const Root = () => {
   return (
-
-  )
+    <>
+      <Navbar />
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </>
+  );
 };
 
 export default Root;
