2854da71149a8b9eb6c94c9eb50c691c15aeb845
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 16:00:57 +0600

    build: layout of component

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,15 +3,20 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
+import { Box, Container, Stack } from "@mui/material";
 
 const Root = () => {
   return (
-    <>
+    <Box>
       <Navbar />
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-    </>
+      <Container maxWidth="lg">
+        <Stack justifyContent="space-evenly" direction="row" spacing={2}>
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 
