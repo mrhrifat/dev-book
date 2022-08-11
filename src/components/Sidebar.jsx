7d1536c7eee8c0d2051a45316c423fc19ab44aff
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 18:03:16 +0600

    build: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,71 +6,81 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  createTheme,
 } from "@mui/material";
 import { Home } from "@mui/icons-material";
+import { ThemeProvider } from "@mui/system";
+
+const theme = createTheme({
+  item: {
+    padding: 0,
+  },
+});
 
 const Sidebar = () => {
   return (
-    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <List>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Pages" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Groups" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Marketplace" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Friends" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton component="a" href="#">
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary="Profile" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </ThemeProvider>
   );
 };
 
