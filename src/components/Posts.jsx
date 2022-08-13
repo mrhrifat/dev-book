443e3767c9a2f11940d977a15c73ced24032d571
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-13 10:31:15 +0600

    build: post card

---
+++ src/components/Posts.jsx
@@ -0,0 +1,50 @@
+import { Favorite, MoreVert, Share } from "@mui/icons-material";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const Posts = (props) => {
+  const { image, username, date, content } = props;
+  return (
+    <Card sx={{ marginBottom: 5 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "pink" }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVert />
+          </IconButton>
+        }
+        title={username}
+        subheader={date}
+      />
+      <CardMedia component="img" height="20%" image={image} alt={image} />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {content}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Favorite />
+        </IconButton>
+        <IconButton aria-label="share">
+          <Share />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Posts;
