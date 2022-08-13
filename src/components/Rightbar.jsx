aedbb4d5d9a9637977c8700620d0f9e8ca0c8097
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-13 11:45:40 +0600

    build: udpate responsive

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -6,7 +6,7 @@ import LatestConversation from "./LatestConversation";
 
 const Rightbar = () => {
   return (
-    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+    <Box flex={2} p={2} sx={{ display: { sm: "none", md: "block" } }}>
       <Box position="fixed">
         <ActivePeople />
         <LatestPost />
