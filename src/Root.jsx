0396ddbe064315da1ad628a72108bcff6f00e5b4
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-13 14:32:30 +0600

    build: theming

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import { Box, Container, CssBaseline, Stack } from "@mui/material";
 import AddPost from "./components/AddPost";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const Root = () => {
+  const [mode, setMode] = useState("light");
+
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <Box>
-      <CssBaseline />
-      <Navbar />
-      <Container maxWidth="lg">
-        <Stack direction="row" spacing={2} justifyContent="center">
-          <Sidebar />
-          <Feed />
-          <Rightbar />
-        </Stack>
-      </Container>
-      <AddPost />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box bgcolor={"background.default"} color={"color.primary"}>
+        <CssBaseline />
+        <Navbar />
+        <Container maxWidth="lg">
+          <Stack direction="row" spacing={2} justifyContent="center">
+            <Sidebar setMode={setMode} mode={mode} />
+            <Feed />
+            <Rightbar />
+          </Stack>
+        </Container>
+        <AddPost />
+      </Box>
+    </ThemeProvider>
   );
 };
 
